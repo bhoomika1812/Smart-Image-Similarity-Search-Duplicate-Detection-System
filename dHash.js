fe50d00ab387a7a9e45ee07d2b1c12c990b806f8
Bhoomika Mittal <bhoomikam.mittal@gmail.com>
2026-07-08 17:54:58 +0530

    updated dHash.js

--- dHash.js
+++ dHash.js
@@ -14,8 +14,25 @@ function generateDHash(grayscaleValues) {
 
     let hash = "";
 
-    return hash;
+    for (let row = 0; row < 8; row++) {
 
+        const rowStart = row * 9;
+
+        for (let col = 0; col < 8; col++) {
+
+            const left = grayscaleValues[rowStart + col];
+            const right = grayscaleValues[rowStart + col + 1];
+
+            if (left < right) {
+                hash += "1";
+            } else {
+                hash += "0";
+            }
+        }
+    }
+
+    return hash;
 }
+
 
 
